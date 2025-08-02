55fcbae09069aee4a0d1df292f06b3010f8aa512
aslahtp <aslahtp47@gmail.com>
2025-08-03 00:55:32 +0530

    removed dotenv from frontend

--- src/hooks/useQuestion.js
+++ src/hooks/useQuestion.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 export const useQuestion = (questionId = 1) => {
     const [title, setTitle] = useState("");
