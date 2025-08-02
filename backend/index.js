f3e72d1266ae0c510c0e98ce35d4d020fc1b60ab
aslahtp <aslahtp47@gmail.com>
2025-08-03 00:00:05 +0530

    env support

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const { VM } = require("vm2");
 const questions = require("./src/questions");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
 
@@ -197,6 +200,6 @@ app.post("/submit/:id", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
